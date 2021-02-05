39ba9debd3b7a715ee78e09a54669a23ff0df889
Sara Abadia <sara.abadia@gmail.com>
2021-02-05 10:10:23 +0000

    C-hello_world commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: puts "Programming is like building a multilingual puzzle"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char texto[] = "''Programming is like building a multilingual puzzle";
+puts(texto);
+return (0);
+}
