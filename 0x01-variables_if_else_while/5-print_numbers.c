1743df7290a6f183645b46f871a9e02164cd9954
Sara Abadia <sara.abadia@gmail.com>
2021-02-06 04:42:30 +0000

    C-variables_if_else_while commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
