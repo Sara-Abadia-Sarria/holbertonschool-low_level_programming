77d5e71e856704075b177aa867a204227fef3b72
Sara Abadia <sara.abadia@gmail.com>
2021-02-06 04:54:18 +0000

    C-variables_if_else_while commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+putchar(alphabet);
+putchar('\n');
+return (0);
+}
