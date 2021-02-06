d014b3ba86429fb9a2d4719de1a3689be7c1b1f4
Sara Abadia <sara.abadia@gmail.com>
2021-02-06 01:11:56 +0000

    C-variables_if_else_while commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 /* more headers goes there */
 
+/**
+ * main - Entry point
+ *
+ * Description: printf the variable n is positive or negative
+ * Return: Always 0 (Success)
+ */
+
 /* betty style doc for function main goes there */
 int main(void)
 {
