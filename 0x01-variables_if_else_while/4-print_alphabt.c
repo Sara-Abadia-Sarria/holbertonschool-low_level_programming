813b95079c2538bf355b4dc482cf05d9ab7c2c55
Sara Abadia <sara.abadia@gmail.com>
2021-02-06 04:17:14 +0000

    C-variables_if_else_while commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet != 'q' || alphabet != 'e')
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
